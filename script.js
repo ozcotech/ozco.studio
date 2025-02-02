5a5beca212c42decf5ce8781bc23a2591120fe8d
ozcotech <ozkancomert@hotmail.com>
2025-02-02 22:36:48 +0300

    Navbar added and updated index.html, css files ans script file.

--- script.js
+++ script.js
@@ -92,3 +92,16 @@ function copyToClipboard(elementId, messageId) {
         console.error("Copy failed!", err);
     });
 }
+
+// Navbar Scroll Effect
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector("nav");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+
